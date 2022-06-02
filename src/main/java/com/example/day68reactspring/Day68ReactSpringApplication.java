1c857d12c1787bb430b0093c4a3d8242859b5902
Yusuf Akin <kutluhanakin@gmail.com>
2022-06-02 15:09:11 -0400

    Initial commit

---
+++ src/main/java/com/example/day68reactspring/Day68ReactSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.day68reactspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day68ReactSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day68ReactSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/day68reactspring/Day68ReactSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day68reactspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day68ReactSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
